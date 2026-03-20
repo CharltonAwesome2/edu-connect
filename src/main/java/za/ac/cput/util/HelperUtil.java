b09426de4ccd8c21f7bf496270527c042132e50f
CharltonAwesome2 <220483418@mycput.ac.za>
2026-03-20 08:00:08 +0200

    Update files to reflect newest origin/main structure

--- src/main/java/za/ac/cput/util/HelperUtil.java
+++ src/main/java/za/ac/cput/util/HelperUtil.java
@@ -2,11 +2,6 @@ package za.ac.cput.util;
 
 import org.apache.commons.validator.routines.EmailValidator;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import za.ac.cput.domain.Subject;
-import za.ac.cput.domain.TutorProfile;
-import za.ac.cput.domain.User;
-
-import java.time.LocalDateTime;
 import java.util.UUID;
 
 public class HelperUtil {
@@ -28,39 +23,18 @@ public class HelperUtil {
         return Math.abs(UUID.randomUUID().getMostSignificantBits());
     }
 
-    public static boolean isInvalidInput(LocalDateTime sessionStart,
-                                       LocalDateTime sessionEnd,
-                                       String sessionStatus,
-                                       TutorProfile tutorProfile,
-                                       User student,
-                                       Subject subject){
-        return sessionStart == null ||
-               sessionEnd == null ||
-               HelperUtil.isNullOrEmpty(sessionStatus) ||
-               tutorProfile == null ||
-               student == null ||
-               subject == null ||
-               sessionEnd.isBefore(sessionStart);
-
+    public static boolean isPositiveDouble(double inputDouble) {
+        return inputDouble > 0;
     }
 
-    public static boolean isInvalidTutorSubject(TutorProfile tutorProfile,
-                                                Subject subject){
-        return   tutorProfile == null ||
-                subject == null;
+    public static boolean isZeroOrPositiveInteger(int inputInteger) {
+        return inputInteger >= 0;
     }
-    public static boolean isValidHourlyRate(double hourlyRate){
-        return hourlyRate > 0;
-    }
-    public  static boolean isValidYearsOfExperience(int yearsExperience){
-        return yearsExperience >= 0;
-    }
-    public static boolean isValidUser(User user){
-        return user != null;
-    }
-    public static boolean isValidRating(double rating){
+
+    public static boolean isValidRating(double rating) {
         return rating >= 0 & rating <= 5;
     }
+
     public static String hashPassword(String password) {
         return encoder.encode(password);
     }
