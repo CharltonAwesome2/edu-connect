36773ac8b25b08a0d0301ebeb436e8dccc3a80c3
tshab <240263952@mycput.ac.za>
2026-03-23 18:24:35 +0200

    Commit message

--- src/main/java/za/ac/cput/factory/userAuthFactory.java
+++ src/main/java/za/ac/cput/factory/userAuthFactory.java
@@ -25,10 +25,12 @@ public class userAuthFactory {
         }
 
         String hashedPassword = HelperUtil.hashPassword(password);
+        String salt = HelperUtil.extractSalt(hashedPassword);
 
         return new UserAuth.Builder()
                 .setAuthId(HelperUtil.generateId())
                 .passwordHash(hashedPassword)
+                .setSalt(salt)
                 .setCreatedAt(LocalDateTime.now())
                 .setUser(user)
                 .build();
